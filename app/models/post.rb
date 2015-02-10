788c3b46d76a3532718a17db1f14cda47f90b178
Johnny Ji <johnny.ji@live.ca>
2015-02-09 16:32:38 -0800

    added paperclip images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+	has_attached_file :image, styles: { medium: "750x500#", thumb: "350x350>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
