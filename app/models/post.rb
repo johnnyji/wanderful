15804aa479f854e3562cd427d756e408aec8ea5c
Johnny Ji <johnny.ji@live.ca>
2015-02-10 19:20:19 -0800

    added comments counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: { medium: "750x500#", thumb: "350x350#" }
+	has_attached_file :image, styles: { medium: "750x500#", thumb: "250x250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_presence_of :title, :link, :description
 end
