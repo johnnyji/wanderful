2d7450558e69e29bac1c744914f109adee7b9b90
Johnny Ji <johnny.ji@live.ca>
2015-02-13 12:51:38 -0800

    styled comments and added delete comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = Comment.create(params[:comment].permit(:content))
+		@comment = Comment.create(comment_params)
 		@comment.user_id = current_user.id
 		@comment.post_id = @post.id
 
@@ -12,6 +12,22 @@ class CommentsController < ApplicationController
 		else
 			render "new"
 		end
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		if @comment.destroy
+			redirect_to post_path(@comment.post)
+			flash.notice = "Comment successfully deleted!"
+		else
+			flash.notice = "Something went wrong... Hmmm"
+		end
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:content)
 	end
 	
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-end
+  validates :comment, presence: :true
+ end
