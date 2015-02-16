41a9527cd3167144b1b761df4c4eae49adcf7c49
Johnny Ji <johnny.ji@live.ca>
2015-02-16 15:01:39 -0800

    added default image to paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,9 @@ class Post < ActiveRecord::Base
 	
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: { medium: "750x500#", thumb: "250x250#" }
+	has_attached_file :image, styles: { medium: "750x500#", thumb: "250x250#" }, default_url: "/images/:style/imageMissing.jpg"
 
+	#gets image using MetaInspector before the post saves
 	before_save :get_image_from_link, if: ->(post) { post.link_changed? }
 
 	before_validation :format_url
@@ -22,6 +23,7 @@ class Post < ActiveRecord::Base
 	end
 
 	private
+	#defines the MetaInspector method
 	def get_image_from_link
 		page = MetaInspector.new(link)
 		#ends the method if the page doesnt have any images
