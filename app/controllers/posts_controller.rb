db22139b06080281df197625359f5a06624e223a
Johnny Ji <johnny.ji@live.ca>
2015-02-28 11:58:43 -0800

    commented update method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,11 +44,11 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		@post.attributes = post_params
+		@post.attributes = post_params # sets the post attributes to the attributes present in the edit view
 		current_tags = @post.tag_list
-		current_tags.remove(current_tags)
-		tags = @post.extract_tags
-		tags.each { |tag| @post.tag_list.add(tag) }
+		current_tags.remove(current_tags) # clears the tag list
+		tags = @post.extract_tags # extracts tags from the edit view description set by the @post.attributes
+		tags.each { |tag| @post.tag_list.add(tag) } # adds each tag to the tag list
 		
 		if @post.save(post_params)
 			redirect_to @post
