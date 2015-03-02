e26088244aa940d92c60343c8b666461e867f92d
Johnny Ji <johnny.ji@live.ca>
2015-03-01 17:22:51 -0800

    added amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV["WANDERFULLY-PRODUCTION"],
+      access_key_id: ENV["AKIAI57ZYBWUPX5JRCFQ"],
+      secret_access_key: ENV["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"]
+    }
+  }
 end
