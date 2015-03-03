fb48fa57842d999d3dfd59a42069bf186eee3f44
Johnny Ji <johnny.ji@live.ca>
2015-03-02 17:28:18 -0800

    added username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,17 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  # anytime you save a user, check for @ then remove if @ is present
+  before_save :check_username
+
   # adding user image using paperclip
   has_attached_file :image, styles: { medium: "200x200#", thumb: "50x50#"}, :default_url => ActionController::Base.helpers.asset_path('missing_:style.png')
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   # search
   scope :search, lambda {|search_terms| where(["lower(username) like ?", "%#{search_terms.downcase}%"])}
+
+  def check_username
+    self.username.gsub!(/^\@*/, "")
+  end
 end
