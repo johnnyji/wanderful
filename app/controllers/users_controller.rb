f1212de09fa2d84329380032143cf8c238028897
Johnny Ji <johnny.ji@live.ca>
2015-03-03 11:49:01 -0800

    removed unneccesary lines from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,32 +6,16 @@ class UsersController < ApplicationController
 
 	def follow
 		@user = User.find(params[:id])
-		if current_user
-			if current_user == @user
-				flash.alert = "You can't follow yourself!"
-			else
-				current_user.follow(@user)
-				redirect_to :back
-				#somehow notify the @user that he/she has been followed
-				flash.notice = "You are following @#{@user.username}"
-			end
-		else
-			flash.alert = "You must <a href='/users/sign_in'>login</a> in order to follow @#{@user.username}".html_safe
-		end
+			current_user.follow(@user)
+			redirect_to :back
+			#somehow notify the @user that he/she has been followed
+			flash.notice = "You are following @#{@user.username}"
 	end
 
 	def unfollow
 		@user = User.find(params[:id])
-		if current_user
-			if current_user == @user
-				flash.alert = "You can't unfollow yourself!"
-			else
-				current_user.stop_following(@user)
-				redirect_to :back
-				flash.notice = "You have stopped following @#{@user.username}"
-			end
-		else
-			flash.alert = "You must <a href='/users/sign_in'>login</a> in order to follow @#{@user.username}".html_safe
-		end
+			current_user.stop_following(@user)
+			redirect_to :back
+			flash.notice = "You have stopped following @#{@user.username}"
 	end
 end
