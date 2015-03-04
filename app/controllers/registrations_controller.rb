763d7630460de3faee969e5c87d6379ea780b489
Johnny Ji <johnny.ji@live.ca>
2015-03-03 23:15:22 -0800

    began adding username validations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
 	respond_to :html, :js, :json
+
+	def create
+		#write code that will check the user's username validity
+		super # runs the rest of the devise registration code
+	end
+
+	def update
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,25 +6,38 @@ class User < ActiveRecord::Base
 
   # extend FriendlyId
   # friendly_id :username
-  
+
+  FORMAT_USERNAME_REGEX = /^\d*[a-zA-Z][a-zA-Z\d]*$/i
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   acts_as_follower
   acts_as_followable
 
-  # anytime you save a user, check for @ then remove if @ is present
-  before_save :check_username
+  # anytime you save a user, check username for REGEX match
+  before_save :remove_username_prefix
 
   # adding user image using paperclip
   has_attached_file :image, styles: { medium: "200x200#", thumb: "50x50#"}, :default_url => ActionController::Base.helpers.asset_path('missing_:style.png')
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  # search
   scope :search, lambda {|search_terms| where(["lower(username) like ?", "%#{search_terms.downcase}%"])}
 
-  def check_username
-    self.username.gsub!(/^\@*/, "")
-    # CHECKS IF THE USERNAME HAS ANY PUNCTUATIONS OR SPACES, IF IT DOESNT, RAISE ERROR 
+
+  def remove_username_prefix
+    self.username.gsub!(/^\@*/i, "")
   end
+
+  def check_username_format
+    if self.username.match(FORMAT_USERNAME_REGEX)
+      #username is valid
+      return true
+    else
+      #username is invalid
+      return false
+    end
+  end
+
+
 end
